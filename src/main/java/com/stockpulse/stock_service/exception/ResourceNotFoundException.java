025bf4def97154eb6c8da57314f24bf71ab736b1
adit mittal <adit11041998@gmail.com>
2025-07-13 18:43:18 +0530

    minor fix

--- src/main/java/com/stockpulse/stock_service/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.stockpulse.stock_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
